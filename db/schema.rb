4e89aba25de08107da5394bebc438d3947e885be
shar0ff <sharovivan17@gmail.com>
2025-02-08 20:28:49 +0100

    Add draft, scheduled and published states of Blog Posts based on published_at field

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_08_160420) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_08_183909) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
